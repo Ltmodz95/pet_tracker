472e66004fcf6d6f7f731ac4464d12934e988e09
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2024-06-20 18:56:54 +0200

    removing the db as it is only an in memory application

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_619_193_913) do
-  create_table 'pets', force: :cascade do |t|
-    t.string 'pet_type'
-    t.string 'tracker_type'
-    t.integer 'owner_id'
-    t.boolean 'in_zone'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
