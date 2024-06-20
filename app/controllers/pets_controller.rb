0ae19e71ea8a6f67ec2a7331de56a92b2277e7f4
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2024-06-20 12:31:30 +0200

    Fixing errors

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
+# Restful controller for Pet resource
 class PetsController < ApplicationController
+  def index
+    render json: Pet.all
+  end
+
+  def create
+    render json: Pet.create(pet_params)
+  end
+
+  def show
+    render json: Pet.find(params[:id].to_i)
+  end
+
+  def out_of_zone
+    Pet.all
+  end
+
+  private
+
+  def pet_params
+    params.permit(:pet_type, :tracker_type, :owner_id, :lost_tracker, :in_zone)
+  end
 end
--- app/models/application_memory.rb
+++ app/models/application_memory.rb
@@ -5,46 +5,47 @@
 # Such as pets & pets_tracker_status, since we only have the lost_tracker available
 # for cats, so we have created a separate hashmap the is liked to the pet through it's id
 # which acts as a belongs_to/has_one relationship in the database
-
 class ApplicationMemory
   include ActiveModel::Model
   include ActiveModel::Validations
 
-  @pets = {}.with_indifferent_access
-  @pets_tracker_status = {}.with_indifferent_access
-  @pet_id = 1
-  @tracker_id = 1
+  def initialize(attributes = [])
+    attributes.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
 
   class << self
+    def pets = @pets ||= {}
+    def pets_tracker_status = @pets_tracker_status ||= {}
+
     def create(attrs)
       lost_tracker = attrs.delete(:lost_tracker)
       pet = Pet.new(**attrs)
 
       return unless pet.valid?
 
-      id = @pet_id
-      @pets[id] = { id: @pet_id }.merge(**attrs)
-      @pet_id += 1
+      id = pets.count + 1
+      pets[id] = { id: }.merge(**attrs)
 
       if pet.pet_type == 'cat' && !lost_tracker.nil?
-        @pets_tracker_status[id] = { id: @tracker_id, lost_tracker: }
-        @tracker_id += 1
+        tracker_id = pets_tracker_status.count(+1)
+        pets_tracker_status[id] = { id: tracker_id, lost_tracker: }
+        return pets[id].merge({ lost_tracker: })
       end
 
-      return @pets[id].merge({ lost_tracker: @pets_tracker_status[id][:lost_tracker] }) if @pets_tracker_status[id]
-
-      @pets[id]
+      pets[id]
     end
 
     def find(id)
-      return @pets[id].merge({ lost_tracker: @pets_tracker_status[id] }) if @pets_tracker_status[id]
+      return pets[id].merge({ lost_tracker: pets_tracker_status[id] }) if pets_tracker_status[id]
 
-      @pets[id]
+      pets[id]
     end
 
     def all
-      @pets.map do |key, val|
-        @pets_tracker_status[key] ? val.merge(lost_tracker: @pets_tracker_status[key][:lost_tracker]) : val
+      pets.map do |key, val|
+        pets_tracker_status[key] ? val.merge(lost_tracker: pets_tracker_status[key][:lost_tracker]) : val
       end
     end
   end
